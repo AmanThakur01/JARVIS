9802a0ac2e4c2ec45e6b3b4f5cbc68f5c1da3d67
AmanThakur01 <amanmrthakur@gmail.com>
2021-06-18 11:52:40 +0530

    18

--- weather/main-app.py
+++ weather/main-app.py
@@ -15,6 +15,7 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 
 from weather.main_gui import Ui_MainWindow
 
+global data_dict, new_df
 url = "https://api.openweathermap.org/data/2.5/forecast?q=bhopal&appid=ef831cc525f939c8eb6824aa48c69c82"
 req = requests.get(url)
 text_data = req.text
@@ -27,13 +28,15 @@ weather = df.weather
 dt_txt = df.dt_txt
 df2 = pd.DataFrame(main)
 data_dict = {"feels_like": [], "temp_min": [], "temp_max": [], 'temp': [], "humidity": [], "dt_txt": [], 'cloud': [],
-             'type': []}
+             'cloud_type': []}
 arts = weather[0][0]['main']
 temperature = main[0]['temp']
 humidity = main[0]["humidity"]
 
 
 class MainThread(Thread):
+    global data_dict, new_df
+
     def run(self):
         for t in dt_txt:
             data_dict["dt_txt"].append(t)
@@ -45,7 +48,8 @@ class MainThread(Thread):
             data_dict['humidity'].append(i['humidity'])
         for w in weather:
             data_dict['cloud'].append(w[0]['main'])
-            data_dict['type'].append(w[0]['description'])
+            data_dict['cloud_type'].append(w[0]['description'])
+        # print(data_dict)
 
 
 startExecution = MainThread()
@@ -60,76 +64,72 @@ class Main(QMainWindow):
         self.ui.pushButton_2.clicked.connect(self.close)
 
     def draw(self):
+        global data_dict, new_df
         # x = np.random.normal(size=1000)
         # y = np.random.normal(size=(3, 1000))
         # for i in range(3):
         #     self.ui.graphicsView.plot(x, y[i], pen=(i, 3))
-        new_df = pd.DataFrame(data_dict)
-
+        print(data_dict)
+        # print(new_df)
+        # print(new_df["cloud_type"])
         sns.set_theme(style="darkgrid")
         plt.figure(figsize=(25, 10), )
-        plot = sns.lineplot(x="type", y="dt_txt", hue='cloud',
-                            data=new_df)
-        plt.setp(plot.get_xticklabels(), rotation=10)
-        self.ui.graphicsView.plot(x="type", y="dt_txt",data=new_df)
+        # plot = sns.lineplot(x="type", y="dt_txt", hue='cloud',
+        #                     data=new_df)
+        # plt.setp(plot.get_xticklabels(), rotation=10)
+        self.ui.graphicsView.plot(kind="scatter", x=data_dict["temp"], y=data_dict["feels_like"])
 
-    def draw2(self):
-        new_df = pd.DataFrame(data_dict)
-
-        sns.set_theme(style="white", context="talk")
-        # rs = np.random.RandomState(8)
-
-        # Set up the matplotlib figure
-        f, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(24, 15))  # , sharex=True
-
-        # Generate some sequential data
-        # y1 = np.arange(1, 11)
-        plot1 = sns.barplot(x=new_df['temp'], y=new_df['type'], palette="Greens_d", ax=ax1, data=new_df)
-        ax1.axhline(0, color="k", clip_on=False)
-        ax1.set_ylabel("few clouds,\nscattered clouds,\nclear sky,\nbroken clouds,\novercast clouds,\nlight rain")
-        # Center the data to make it diverging
-        # y2 = y1 - 5.5
-        plot2 = sns.barplot(x=new_df['temp'], y=new_df['cloud'], palette="vlag", ax=ax2, data=new_df)
-        ax2.axhline(0, color="k", clip_on=False)
-        # ax2.set_ylabel("cloud")
-
-        # Randomly reorder the data to make it qualitative
-        # y3 = rs.choice(y1, len(y1), replace=False)
-        plot3 = sns.barplot(x=round(new_df['temp']), y=new_df['humidity'], palette="deep", ax=ax3, data=new_df)
-        ax3.axhline(0, color="k", )
-        # ax3.set_ylabel("humidity")
-
-        # Finalize the plot
-        sns.despine(bottom=True)
-        plt.setp(plot1.get_yticklabels(), rotation=180)
-        plt.setp(plot1.get_xticklabels(), rotation=75)
-        plt.setp(plot2.get_xticklabels(), rotation=75)
-        plt.setp(plot3.get_xticklabels(), rotation=75)
-
-        plt.setp(f.axes, yticks=[])
-        plt.tight_layout(h_pad=2)
-        # plt.show()
-
+    # def draw2(self):
+    #     new_df = pd.DataFrame(data_dict)
+    #
+    #     sns.set_theme(style="white", context="talk")
+    #     # rs = np.random.RandomState(8)
+    #
+    #     # Set up the matplotlib figure
+    #     f, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(24, 15))  # , sharex=True
+    #
+    #     # Generate some sequential data
+    #     # y1 = np.arange(1, 11)
+    #     plot1 = sns.barplot(x=new_df['temp'], y=new_df['type'], palette="Greens_d", ax=ax1, data=new_df)
+    #     ax1.axhline(0, color="k", clip_on=False)
+    #     ax1.set_ylabel("few clouds,\nscattered clouds,\nclear sky,\nbroken clouds,\novercast clouds,\nlight rain")
+    #     # Center the data to make it diverging
+    #     # y2 = y1 - 5.5
+    #     plot2 = sns.barplot(x=new_df['temp'], y=new_df['cloud'], palette="vlag", ax=ax2, data=new_df)
+    #     ax2.axhline(0, color="k", clip_on=False)
+    #     # ax2.set_ylabel("cloud")
+    #
+    #     # Randomly reorder the data to make it qualitative
+    #     # y3 = rs.choice(y1, len(y1), replace=False)
+    #     plot3 = sns.barplot(x=round(new_df['temp']), y=new_df['humidity'], palette="deep", ax=ax3, data=new_df)
+    #     ax3.axhline(0, color="k", )
+    #     # ax3.set_ylabel("humidity")
+    #
+    #     # Finalize the plot
+    #     sns.despine(bottom=True)
+    #     plt.setp(plot1.get_yticklabels(), rotation=180)
+    #     plt.setp(plot1.get_xticklabels(), rotation=75)
+    #     plt.setp(plot2.get_xticklabels(), rotation=75)
+    #     plt.setp(plot3.get_xticklabels(), rotation=75)
+    #
+    #     plt.setp(f.axes, yticks=[])
+    #     plt.tight_layout(h_pad=2)
+    #     # plt.show()
     def startjarvis(self):
+        startExecution.start()
         # main gif run
         self.ui.movie = QtGui.QMovie(
             "/home/aman/PycharmProjects/weather/thunder.gif")
         self.ui.label.setMovie(self.ui.movie)
         self.ui.movie.start()
-        self.draw()
-        # # initlization gif run
-        # self.ui.movie = QtGui.QMovie(
-        #     "/home/aman/PycharmProjects/JARVIS/data/inslization.gif")
-        # self.ui.label_3.setMovie(self.ui.movie)
-        # self.ui.movie.start()
 
         timer = QTimer(self)
         timer.timeout.connect(self.showtime)
         timer.start(1000)
-        startExecution.start()
+        self.draw()
 
     def showtime(self):
-        global f
+        # global f
         # datetime.datetime.now().strftime("%H hour and %M minutes")
         # textbox=background:transparent;\nfont: 75 11pt "Purisa";\ncolor:white;
         self.ui.textBrowser_2.setText(
